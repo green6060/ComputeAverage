d6ea4a371f622df822268f0521d18788a3abd81b
Dallas Parker <dallasjamesparker@gmail.com>
2018-11-16 13:23:06 -0700

    add feature average exactly 3 numbers given by user

--- src/computeaverage/ComputeAverage.java
+++ src/computeaverage/ComputeAverage.java
@@ -9,6 +9,29 @@ package demo;
  *
  * @author Dallas
  */
+
+import java.util.Scanner;
+
 public class ComputeAverage {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        try {
+        
+            System.out.print("Enter three numbers: ");
+            double num1 = input.nextDouble();
+            double num2 = input.nextDouble();
+            double num3 = input.nextDouble();
+            
+            double average = ((num1 + num2 + num3) / 3);
+            
+            System.out.println("The average of " + num1 + " " + num2 + " " + num3 + 
+            " is " + average);
+            
+        } finally {
+            input.close();
+        }
+    }
     
 }
